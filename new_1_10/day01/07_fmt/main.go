a335de42d21af477812fb13367c0b9fb879f7dc7
MonsterDragon <1962610298@qq.com>
2021-01-14 19:22:08 +0800

    格式化

---
+++ new_1_10/day01/07_fmt/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	var n = 100
+	fmt.Printf("%T\n", n)
+	fmt.Printf("%v\n", n)
+	fmt.Printf("%d\n", n)
+	fmt.Printf("%o\n", n)
+	fmt.Printf("%x\n", n)
+	fmt.Printf("%b\n", n)
+	fmt.Printf("%T\n", n)
+
+	s := "HELLO shuzhan"
+	fmt.Printf("%v\n", s)
+}
