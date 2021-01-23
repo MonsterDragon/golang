d030ea6405c5696b968d5ba346ea308bcdbb7374
MonsterDragon <1962610298@qq.com>
2021-01-23 10:19:12 +0800

    update splite

--- new_1_10/day03/03_splice/main.go
+++ new_1_10/day03/03_splice/main.go
@@ -37,7 +37,10 @@ func main() {
 	fmt.Printf("len(s3)=%d, cap(s3)=%d\n", len(s3), cap(s3))
 	fmt.Printf("len(s4)=%d, cap(s4)=%d\n", len(s4), cap(s4))
 	fmt.Printf("len(s5)=%d, cap(s5)=%d\n", len(s5), cap(s5))
-
+	fmt.Println(s5)
+	// 切片是引用类型
 	// 切片再切片
-
+	s7 := s5[1:3]
+	fmt.Println(s7)
+	fmt.Printf("len(s7)=%d, cap(s7)=%d\n", len(s7), cap(s7))
 }
