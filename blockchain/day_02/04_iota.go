11daf0bb77fd2cdd79aaddc0032850b4080e11b4
MonsterDragon <1962610298@qq.com>
2021-03-19 20:46:40 +0800

    update iota file

--- blockchain/day_02/04_iota.go
+++ blockchain/day_02/04_iota.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main_01() {
 	const(
-		a = iota
+		a = iota // 常量自动生成器
 		b = iota
 		c = iota
 		d = iota
@@ -27,9 +27,11 @@ func main02() {
 func main() {
 	const(
 		a = 10
-		b, c = iota, iota
-		d, e
+		b, c = iota, iota // 1, 1 同一行的iota相同
+		d, e // 2, 2
 	)
 
 	fmt.Println(a, b, c, d, e)
+	const f = iota
+	fmt.Println(f) // 1 在一个const声明语句中，在第一个声明的常量所在的行，iota会被重置为0
 }
